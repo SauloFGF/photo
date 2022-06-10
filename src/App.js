3dfc6d1b83b730ecf6026516e4b8f0cfa865f8cf
Saulo Figueiredo <saulo.fgf21@gmail.com>
2022-06-10 14:51:49 -0300

    att 1.05

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import vector from './assets/icon/Vector.svg'
 
 
 
-
-
 const imagens = ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg', 'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426__340.jpg',
     'https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522__340.jpg', 'https://cdn.pixabay.com/photo/2016/09/08/13/58/desert-1654439__340.jpg',
     'https://cdn.pixabay.com/photo/2016/10/14/19/21/canyon-1740973__340.jpg', "https://cdn.pixabay.com/photo/2016/11/29/04/19/ocean-1867285__340.jpg", "https://cdn.pixabay.com/photo/2017/02/15/11/15/wintry-2068298__340.jpg",
@@ -33,14 +31,9 @@ const App = () => {
     }
 
     function filterapp() {
-        if (atr === 'blur') {
-            return `${atr}(${brig}px)`
-        }
-        else if (atr === "hue-rotate") {
-            return `${atr}(${brig}deg)`
-        } else {
-            return `${atr}(${brig}%)`
-        }
+        return (atr === 'blur') ? `${atr}(${brig}px)`
+            : (atr === "hue-rotate") ? `${atr}(${brig}deg)`
+                : `${atr}(${brig}%)`
     }
 
     return (
@@ -48,53 +41,59 @@ const App = () => {
             <div className="conteiner">
                 <div className="subConteiner1">
                     <div className="viewer">
+                        <button className="arrow">{"<"}</button>
                         <img className="viewerImg" src={image} alt='' style={{ filter: filterapp() }} />
+                        <button className="arrow">{">"}</button>
                     </div>
-                    <div className="bars">
-                        {imagens.map(function (image) {
-                            return <img onClick={() => {
-                                setImage(image)
-                                setBlur({ value: "0" })
-                            }} src={image} alt='' />
-                        })}
+                    <div className="navBar">
+                        <button className="arrow">{"<"}</button>
+                        <div className="bars">
+                            {imagens.map(function (image) {
+                                return <img onClick={() => {
+                                    setImage(image)
+                                    setBlur({ value: "0" })
+                                }} className="imgBar" src={image} alt='' />
+                            })}
+                        </div>
+                        <button className="arrow">{">"}</button>
                     </div>
                 </div>
                 <div className="edit">
                     <h2 className="top">Edição</h2>
                     <div className="scale">
-                        <div className='symbols'><img src={vector} alt="icon"/><h3>Blur</h3></div>
+                        <div className='symbols'><img src={vector} alt="icon" /><h3>Blur</h3></div>
                         <input type='range' min='0' max="10" onChange={handleChangeBlur} data-filter="blur" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={sun} alt="icon"/><h3>Brightness</h3> </div>
-                        <input  type='range' min='0' max="300" onChange={handleChangeBlur} data-filter="brightness" value={blur.value} />
+                        <div className='symbols'><img src={sun} alt="icon" /><h3>Brightness</h3> </div>
+                        <input type='range' min='0' max="300" onChange={handleChangeBlur} data-filter="brightness" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={sliders} alt="icon"/><h3>Contrast</h3></div>
+                        <div className='symbols'><img src={sliders} alt="icon" /><h3>Contrast</h3></div>
                         <input type='range' min='0' max="300" onChange={handleChangeBlur} data-filter="contrast" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={aperture} alt="icon"/><h3>Grayscale</h3></div>
+                        <div className='symbols'><img src={aperture} alt="icon" /><h3>Grayscale</h3></div>
                         <input type='range' min='0' max="100" onChange={handleChangeBlur} data-filter="grayscale" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={repeat} alt="icon"/><h3>Invert</h3></div>
+                        <div className='symbols'><img src={repeat} alt="icon" /><h3>Invert</h3></div>
                         <input type='range' min='0' max="100" onChange={handleChangeBlur} data-filter="invert" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={gitcommit} alt="icon"/><h3>Opacity</h3></div>
+                        <div className='symbols'><img src={gitcommit} alt="icon" /><h3>Opacity</h3></div>
                         <input type='range' min='0' max="100" onChange={handleChangeBlur} data-filter="opacity" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={circle} alt="icon"/><h3>Saturate</h3></div>
+                        <div className='symbols'><img src={circle} alt="icon" /><h3>Saturate</h3></div>
                         <input type='range' min='0' max="300" onChange={handleChangeBlur} data-filter="Saturate" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={copy} alt="icon"/><h3>Sepia</h3></div>
+                        <div className='symbols'><img src={copy} alt="icon" /><h3>Sepia</h3></div>
                         <input type='range' min='0' max="100" onChange={handleChangeBlur} data-filter="sepia" value={blur.value} />
                     </div>
                     <div className="scale">
-                         <div className='symbols'><img src={rotate} alt="icon"/><h3>Hue-Rotate</h3></div>
+                        <div className='symbols'><img src={rotate} alt="icon" /><h3>Hue-Rotate</h3></div>
                         <input type='range' min='0' max="360" onChange={handleChangeBlur} data-filter="hue-rotate" value={blur.value} />
                     </div>
                 </div>
